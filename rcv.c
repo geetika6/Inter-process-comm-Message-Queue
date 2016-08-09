0fe4e06210ba4039e1155ccc57c050f4f0581b77
geetika6 <geetika6@gmail.com>
2016-08-09 12:51:55 +1000

    1 rcv and 1 send process to communicate through Msg Que

---
+++ rcv.c
@@ -0,0 +1,40 @@
+#include "header.h"
+struct my_msg_st 
+{
+	long int my_msg_type;    /* type of msg q */
+        char some_text[BUFSIZ];  /* Buffer for IPC_INFO*/
+};
+int main()
+{
+
+	int running=1;
+	int msgid;
+	struct my_msg_st some_data;
+	long int msg_to_receive=0;
+	msgid=msgget((key_t)1234,0666|IPC_CREAT);
+        if(msgid==-1)
+	{
+		fprintf(stderr,"Failed creating msgget\n");
+		exit(EXIT_FAILURE);
+	}
+	while(running)
+	{
+		if(msgrcv(msgid,(void *)&some_data,BUFSIZ,msg_to_receive,0)==-1)
+		{
+			fprintf(stderr,"Failed msgrcv\n");
+			exit(EXIT_FAILURE);
+		}
+		printf("You wrote :%s",some_data.some_text);
+		if(strncmp(some_data.some_text,"end",3)==0)
+ 		{
+			running=0;
+		}
+	}
+	if (msgctl(msgid, IPC_RMID,0) ==-1)
+	{
+		fprintf(stderr,"Failed Removing msgq\n");
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+
+}
